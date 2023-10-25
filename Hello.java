0d0191579bea72b6051d96e0d744935937e743ae
Akshay <paraba777@gmail.com>
2023-10-25 17:02:11 +0530

    print and datatype

--- Hello.java
+++ Hello.java
@@ -3,7 +3,14 @@ class Hello
 
     public static void main(String a[])
     {
-        System.out.print("Hello World");
+
+        int num1 = 3;   //todo creating varaible - datatype variablename = value
+        int num2 = 6;
+        int result = num1 + num2;
+        System.out.println(result);
+        System.out.println("Hello World");  //**println is used when we print the value to the next line */
+        System.out.print(1+2);
+
     }
 
 }
