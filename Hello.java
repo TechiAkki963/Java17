9e7321967b8c2209b604591812109bb1bc814eb8
Akshay <paraba777@gmail.com>
2023-10-26 02:27:04 +0530

    readme range - primitive datatype

--- Hello.java
+++ Hello.java
@@ -1,43 +1,40 @@
-class Hello
-{
+class Hello {
 
-    public static void main(String a[])
-    {
+    public static void main(String a[]) {
 
-        int num1 = 3;   //todo creating varaible - datatype variablename = value
+        int num1 = 3; // todo creating varaible - datatype variablename = value
         int num2 = 6;
         int result = num1 + num2;
         System.out.println(result);
-        System.out.println("Hello World");  //**println is used when we print the value to the next line */
-        System.out.println(1+2);
+        System.out.println("Hello World"); // **println is used when we print the value to the next line */
+        System.out.println(1 + 2);
 
         // Datatypes
 
-        //Primitive DataType
+        // Primitive DataType
         int num3 = 9;
-        byte by=127;
+        byte by = 127;
         short sh = 558;
-        long lg = 58400l; //whenever we use long value always use 'L' at the end
+        long lg = 58400l; // whenever we use long value always use 'L' at the end
 
-        float g = 5.8f;   //whenever we use float value always use 'f' at the end
+        float g = 5.8f; // whenever we use float value always use 'f' at the end
         double d = 5.8;
 
-        char c = 'k';     //!we cannot put ""
+        char c = 'k'; // !we cannot put ""
 
         boolean b = true;
 
+        // Type Conversion and Casting
+        // ** we cannot change the type of a varaiable but we can change the type of
+        // value */
 
-    
+        byte b1 = 127;
+        int a1 = b1;
+        System.out.println(a1);
 
-        //Type Conversion and Casting
-         
-         byte b1 = 127;
-         int a1 = b1;
-         System.out.print(a1);
-
-        
-
-        
+        float f = 5.6f;
+        int a3 = (int) f;
+        System.out.println(a3); // output : 5 not 5.6
 
     }
 
