df39e455c070210cb449e5ed84ac02c1c5995d73
Akshay <paraba777@gmail.com>
2023-10-26 02:47:33 +0530

    Math Operator and Increment Decrement

--- Hello.java
+++ Hello.java
@@ -36,6 +36,30 @@ class Hello {
         int a3 = (int) f;
         System.out.println(a3); // output : 5 not 5.6
 
+        int num4 = 2;
+        int num5 = 5;
+        int result1 = num4 % num5; // we can also do + - / *
+        System.out.println(result1);
+
+        // todo Increment
+        int num6 = 7;
+        // num6 = num6 + 2;
+        num6 += 2; // 9
+        num6 -= 2; // 7
+        num6 *= 8; // 56
+        num6++; // * 57 Post -Increment */
+        ++num6; // * Pre - Increment*/
+        num6--; // * 56 Decrement */
+        System.out.println(num6); // 9
+
+        // eg for Post and Pre Increment
+        int increment = 7;
+
+        // int result2 = increment++; // 7 as it will first fetch the value and
+        // increment later
+        int result2 = ++increment; // 8 as it first increment the value and fetch later
+        System.out.println(result2);
+
     }
 
 }
