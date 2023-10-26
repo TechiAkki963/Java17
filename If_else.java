d641cfb0de2fde5b5d76e1a9335b77f1374631f8
Akshay <paraba777@gmail.com>
2023-10-26 21:34:24 +0530

    created seperate file for every folder - if else and if else if

--- If_else.java
+++ If_else.java
@@ -1,10 +1,10 @@
-public class Conditional {
+public class If_else {
     public static void main(String a[]) {
 
         int x = 18;
         int y = 10;
 
-        // ? Single Variable condition
+        // ? Single Variable condition - #if else
         // if (x > 10 && x > 20) // * Condition
         // {
         // System.out.println("X is a greater value");
@@ -12,7 +12,9 @@ public class Conditional {
         // System.out.println("Wrong");
         // }
 
-        // ? Multiple Variable Condition
+        // !
+
+        // ? Multiple Variable Condition - #if else
         if (x > y) {
             System.out.println(x);
             System.out.println("Thank You");
---
+++ If_else_if.java
@@ -0,0 +1,27 @@
+public class If_else_if {
+
+    public static void main(String args[]) {
+
+        int x = 8;
+        int y = 7;
+        int z = 16;
+
+        if (x > y && x > z) // todo : check the condition for x
+        {
+            System.out.println(x);
+        }
+        // else if (y > x && y > z) // todo: check the condition for y
+        // {
+        // System.out.println(y);
+        // }
+        else if (y > z) // ? */ as we have already compared the comparison of x and y
+        {
+            System.out.println(y);
+        } else // todo: check the condition for z
+        {
+            System.out.println(z);
+        }
+
+    }
+
+}
