4981d4ceb666f25a35bffff60a649540084c5b7e
Akshay <paraba777@gmail.com>
2023-10-26 17:28:09 +0530

    if else statement

---
+++ Conditional.java
@@ -0,0 +1,25 @@
+public class Conditional {
+    public static void main(String a[]) {
+
+        int x = 18;
+        int y = 10;
+
+        // ? Single Variable condition
+        // if (x > 10 && x > 20) // * Condition
+        // {
+        // System.out.println("X is a greater value");
+        // } else {
+        // System.out.println("Wrong");
+        // }
+
+        // ? Multiple Variable Condition
+        if (x > y) {
+            System.out.println(x);
+            System.out.println("Thank You");
+        } else {
+            System.out.println(y);
+        }
+
+    }
+
+}
--- Operators.java
+++ Operators.java
@@ -15,16 +15,56 @@ public class Operators {
         // eg for Post and Pre Increment
         int increment = 7;
 
-        // int result2 = increment++; // 7 as it will first fetch the value and
-        // increment later
-        int result2 = ++increment; // 8 as it first increment the value and fetch later
-        System.out.println(result2);
+        int result = increment++; // 7 as it will first fetch the value and increment later
+        System.out.println(result);
+
+        int result1 = ++increment; // 8 as it first increment the value and fetch later
+        System.out.println(result1);
 
         // Relational Operator
         int n = 7;
         int m = 5;
         int add = n + m;
-        System.out.println(add);
+        System.out.println(add); // 12
+
+        int sub = n - m;
+        System.out.println(sub); // 2
+
+        int mul = n * m;
+        System.out.println(mul); // 35
+
+        int div = n / m;
+        System.out.println(div); // 1
+
+        // Relation Operator
+
+        int x = 5;
+        int y = 5;
+
+        boolean g = x > y;
+        System.out.println(g); // True
+
+        boolean l = x < y;
+        System.out.println(l); // False
+
+        boolean e = x == y;
+        System.out.println(e); // False
+
+        boolean ne = x != y; // not equal to operator
+        System.out.println(ne); //
+
+        // * Logical Operators
+        // todo : && AND , || OR , ! NOT
+        int a = 7;
+        int b = 5;
+        int c = 5;
+        int d = 9;
+        boolean resulta = a > b && c > d; // false
+        System.out.println(resulta);
+
+        boolean resulto = a < b || c < d || a > 1;
+        System.out.println(resulto); // True
+        System.out.println(!resulto); // False
 
     }
 
